2e3ef5c9a8113b1e8780780bcad7c23f280afbaa
Kilyong Jeong <uzoolove@gmail.com>
2024-07-18 16:15:12 +0900

    ✨

--- workspace-ins/src/server/board/app.ts
+++ workspace-ins/src/server/board/app.ts
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join('views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,12 +24,13 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, `요청하신 페이지를 찾을 수 없습니다.(${req.url})`));
 });
 
 // error handler
 app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction) {
   // set locals, only providing error in development
+  console.error(err.stack);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
--- workspace-ins/src/server/board/routes/index.ts
+++ workspace-ins/src/server/board/routes/index.ts
@@ -3,7 +3,85 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '익스프레스' });
+  const list = [
+    {
+      "_id": 968,
+      "title": "fdgdsf",
+      "content": "sdf",
+      "type": "info",
+      "views": 8,
+      "user": {
+        "_id": 2,
+        "name": "네오",
+        "profile": "/files/00-sample/user-neo.webp"
+      },
+      "createdAt": "2024.07.16 18:20:02",
+      "updatedAt": "2024.07.16 18:20:02",
+      "seller_id": null,
+      "repliesCount": 0,
+      "product": {
+        "image": null
+      }
+    },
+    {
+      "_id": 967,
+      "title": "다시수정다시수정",
+      "content": "존맛탱구리구리구리",
+      "type": "info",
+      "views": 26,
+      "user": {
+        "_id": 82,
+        "name": "매그내틱",
+        "profile": "/files/00-sample/H9g65B9Hr.jpg"
+      },
+      "createdAt": "2024.07.16 15:51:45",
+      "updatedAt": "2024.07.16 15:51:45",
+      "seller_id": null,
+      "repliesCount": 0,
+      "product": {
+        "image": null
+      }
+    },
+    {
+      "_id": 966,
+      "title": "비가 많이온다 호옹",
+      "content": "호옹2",
+      "type": "info",
+      "views": 22,
+      "user": {
+        "_id": 301,
+        "name": "호옹이",
+        "profile": null
+      },
+      "createdAt": "2024.07.16 15:50:09",
+      "updatedAt": "2024.07.16 15:50:09",
+      "seller_id": null,
+      "repliesCount": 1,
+      "product": {
+        "image": null
+      }
+    },
+    {
+      "_id": 964,
+      "title": "헤헤헷 나 된당~~",
+      "content": "룰루 랄라 ~~",
+      "type": "info",
+      "views": 43,
+      "user": {
+        "_id": 300,
+        "name": "동글암이",
+        "profile": "/files/00-sample/iqMn1ugR2.JPG"
+      },
+      "createdAt": "2024.07.16 14:49:21",
+      "updatedAt": "2024.07.16 14:49:21",
+      "seller_id": null,
+      "repliesCount": 4,
+      "product": {
+        "image": null
+      }
+    }
+  ];
+  res.render('index', { list });
 });
 
 export default router;
