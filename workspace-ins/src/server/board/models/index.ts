5b092c0b1929b96209c2133abdedda5d3f757f3d
Kilyong Jeong <uzoolove@gmail.com>
2024-07-19 10:26:51 +0900

    ✨

--- workspace-ins/src/server/board/models/index.ts
+++ workspace-ins/src/server/board/models/index.ts
@@ -4,22 +4,14 @@ const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 const dbName = '00-sample';
 console.log('try to connect...');
-async function main() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
 
-  // the following code examples can be pasted here...
+// Use connect method to connect to the server
+await client.connect();
+console.log('Connected successfully to server');
+const db = client.db(dbName);
+db.post = db.collection('post');
+db.user = db.collection('user');
 
-  return 'done.';
-}
-
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
 
 const model = {
   post: {
